e66e13f65dc13723b5dde9e12d5e39db2b22dc7f
Richard Ewin <rich@Richards-MacBook-Air.local>
2020-08-20 11:43:25 +0100

    GREEN-returns 'Amber: 1', when passed 'Amber'

--- lib/grade_report.rb
+++ lib/grade_report.rb
@@ -1,7 +1,9 @@
 def grade_report(grade)
     if grade === 'Green'
         return 'Green: 1'
-    else 
+    elsif grade === 'Red'
         return 'Red: 1'
+    else
+        return 'Amber: 1'
     end
 end  
