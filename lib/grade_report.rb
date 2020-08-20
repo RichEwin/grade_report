ed6134546d9df4f584227e734620be2f88add874
Richard Ewin <rich@Richards-MacBook-Air.local>
2020-08-20 11:54:03 +0100

    GREEN-returns 'Green: 1\nAmber: 1', when passed 'Green, Amber'

--- lib/grade_report.rb
+++ lib/grade_report.rb
@@ -1,11 +1,5 @@
 def grade_report(grade)
     grades = grade.split(', ')
-    if grades.include?('Green')
-        "Green: #{grades.count('Green')}"
-    elsif grades.include?('Amber') 
-        "Amber: #{grades.count('Amber')}"
-    else 
-        grades.include?('Red')
-        "Red: #{grades.count('Red')}"
-    end
-end    
+    grades.uniq.map { |grade| 
+    "#{grade}: #{grades.count(grade)}"}.join('\n')
+end 
